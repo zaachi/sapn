ea3991175350c08346cfc68e1b0553986dddb5e2
zaachi <zaachi@gmail.com>
2013-02-27 09:30:21 +0100

    delete test code

--- class/sapn.php
+++ class/sapn.php
@@ -241,21 +241,3 @@ class SAPN{
 		return ($this->mode == self::DEVELOPMENT ? $this->_development_url : $this->_production_url );
 	}
 }
-
-
-
-$apns = new SAPN( SAPN::DEVELOPMENT );
-$apns	->setCertificateFile('outCertDevZaachi.pem');
-
-$apns	->addDeviceToken('bf447b59acd20718565b763e2de11382a0db1dc17caec4899fefd9790d0422a4')
-		->addDeviceToken('bf447b59acd20718565b763e2de11382a0db1dc17caec4899fefd9790d0422a4')
-		->addDeviceToken('bf447b59acd20718565b763e2de11382a0db1dc17caec4899fefd9790d0422a5');
-
-$apns	->setBadge(3)
-		->setSound('default');
-//		->setCertificatePassphrase('password');
-
-$apns	->setMessage('this is my push message 3');
-
-$apns	->send();
-?>
